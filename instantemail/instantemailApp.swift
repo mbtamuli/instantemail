6dddd8f8b4dff7732fe00721de1af32be087fb77
Mriyam Tamuli <mbtamuli@gmail.com>
2024-06-22 13:33:09 +0530

    ✨ First commit
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ instantemail/instantemailApp.swift
@@ -0,0 +1,17 @@
+//
+//  instantemailApp.swift
+//  instantemail
+//
+//  Created by Mriyam Tamuli on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct instantemailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
